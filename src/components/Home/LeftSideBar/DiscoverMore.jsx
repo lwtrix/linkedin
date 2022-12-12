837320365490b4e2d11176c2aa30e42f59714e09
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2022-12-12 19:12:47 +0100

    Create DiscoverMore.jsx

---
+++ src/components/Home/LeftSideBar/DiscoverMore.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import { Links } from "./Links";
+import { Buttons } from "./Buttons";
+export const DiscoverMore = () => {
+  return (
+    <div id="discoverMoreStyle">
+      <Links />
+      <Buttons />
+    </div>
+  );
+};
