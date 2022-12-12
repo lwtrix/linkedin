ef6c535bb8343bf9b08678b39de10ee17b16e8e3
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2022-12-12 18:25:52 +0100

    style

--- src/components/Home/LeftSideBar.jsx
+++ src/components/Home/LeftSideBar.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-
+import { DiscoverMore } from "./LeftSideBar/DiscoverMore";
+import { ProfileBar } from "./LeftSideBar/ProfileBar";
+import "../css/homepage/leftSideBar/left-side-bar.css";
 export const LeftSideBar = () => {
-  return <div>LeftSideBar modification</div>;
+  return (
+    <div id="leftSideBarStyle">
+      <ProfileBar />
+
+      <DiscoverMore />
+    </div>
+  );
 };
