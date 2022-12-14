caa59869b13cdb846e151deb7f06b38b679446ba
ArmndoHg <armndohernndez@gmail.com>
2022-12-14 12:20:37 +0100

    update

--- src/components/Home/Homepage.jsx
+++ src/components/Home/Homepage.jsx
@@ -4,15 +4,14 @@ import { Col, Container, Row } from "react-bootstrap";
 import { LeftSideBar } from "./LeftSideBar";
 import { MainContent } from "./MainContent";
 import { NewsSidebar } from "./NewsSidebar";
-import { useSelector } from 'react-redux'
+import { useSelector } from "react-redux";
 
 const Homepage = () => {
-
-  const user = useSelector(state => state.user)
-  console.log(user)
+  const user = useSelector((state) => state.user);
+  console.log(user);
 
   return (
-    <Row className="text-center">
+    <Row className="text-center m-0">
       <Col>
         <LeftSideBar />
       </Col>
