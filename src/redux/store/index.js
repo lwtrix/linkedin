26b7b64533c69db235f19f4df7ca76e031f7d922
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2022-12-15 13:12:54 +0100

    passing chat

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userProfileReducer from "../reducers/UserProfileReducer";
-
+import newChatReducer from "../reducers/NewChatReducer";
 const mainReducer = {
-    user: userProfileReducer
-}
+  user: userProfileReducer,
+  newChat: newChatReducer,
+};
 
 export const store = configureStore({
-    reducer: mainReducer
-})
+  reducer: mainReducer,
+});
